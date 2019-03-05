8539b5657fa84d2dc559b493eb87862b3e243afc
Jonathan Striebel <jstriebel@users.noreply.github.com>
2019-03-05 13:58:26 +0100

    datasource-properties: add largestSegmentId, dataFormat & wkwResolutions (#64)
    * datasource-properties: add largestSegmentId & dataFormat
    
    * make datasource-properties wk backend & frontend compatible

--- wkconnect/webknossos/models.py
+++ wkconnect/webknossos/models.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
-from typing import List, NamedTuple, Tuple
+from typing import Any, List, NamedTuple, Optional, Tuple, cast
 
-from dataclasses import dataclass
+import numpy as np
 
-from ..utils.types import Box3D, Vec3D
+from dataclasses import dataclass, field
+
+from ..utils.types import JSON, Box3D, Vec3D
 
 
 class DataStoreStatus(NamedTuple):
@@ -34,17 +36,27 @@ class DataSourceId(NamedTuple):
     name: str
 
 
-@dataclass(frozen=True)
+@dataclass(unsafe_hash=True)
 class DataLayer:
     name: str
     category: str
     boundingBox: BoundingBox
     resolutions: List[Vec3D]
     elementClass: str
+    dataFormat: str = field(default="wkw", init=False)
+    wkwResolutions: JSON = field(init=False)
+    largestSegmentId: Optional[int] = field(default=None, init=False)
+    mappings: Optional[List[Any]] = field(default=None, init=False)
 
     def __post_init__(self) -> None:
         assert self.category in ["color", "segmentation"]
         assert self.elementClass in ["uint8", "uint16", "uint32", "uint64"]
+        if self.category == "segmentation":
+            self.largestSegmentId = cast(int, np.iinfo(self.elementClass).max)
+            self.mappings = []
+        self.wkwResolutions = [
+            {"resolution": i, "cubeLength": 1024} for i in self.resolutions
+        ]
 
 
 class DataSource(NamedTuple):
