5c66e92903e2fe244658dc2fbedc436c7b11bc59
Jonathan Striebel <jonathan.striebel@gmail.com>
2019-04-30 12:47:56 +0200

    return better error for neuroglancer datasets with missing auth

--- wkconnect/utils/exceptions.py
+++ wkconnect/utils/exceptions.py
@@ -3,6 +3,12 @@ import traceback
 from aiohttp import ClientResponseError
 
 
+class RuntimeErrorWithUserMessage(RuntimeError):
+    def __init__(self, user_message: str):
+        self.user_message = user_message
+        super().__init__(user_message)
+
+
 def exception_traceback(exception: Exception) -> str:
     return "".join(
         traceback.format_exception(
@@ -14,6 +20,8 @@ def exception_traceback(exception: Exception) -> str:
 def format_exception(exception: Exception) -> str:
     if isinstance(exception, ClientResponseError):
         description = f"HTTP {exception.message} ({exception.status})"
+    if isinstance(exception, RuntimeErrorWithUserMessage):
+        return exception.user_message
     else:
         description = type(exception).__name__
     return f"{description} in webknossos-connect"
