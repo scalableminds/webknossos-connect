e80b1183cd05bc5ea47d4914a2a43b25ee7978b2
Florian M <florian@scm.io>
2020-07-29 14:58:31 +0200

    implement basic tif support

--- wkconnect/backends/tiff/backend.py
+++ wkconnect/backends/tiff/backend.py
@@ -1,29 +1,39 @@
 import logging
-from typing import Dict, Optional
+from pathlib import Path
+from typing import Dict, Optional, cast
 
 import numpy as np
 from aiohttp import ClientSession
-from wkcuber import TiffDataset
+from wkcuber.api.Dataset import TiffDataset
 
+from ...utils.types import JSON, Vec3D
 from ..backend import Backend, DatasetInfo
-from ...utils.types import JSON, Box3D, HashableDict, Vec3D, Vec3Df
+from .models import Dataset
 
 logger = logging.getLogger()
 
 
 class Tiff(Backend):
-
     def __init__(self, config: Dict, http_client: ClientSession) -> None:
         super().__init__(config, http_client)
 
     async def handle_new_dataset(
-            self, organization_name: str, dataset_name: str, dataset_info: JSON
+        self, organization_name: str, dataset_name: str, dataset_info: JSON
     ) -> DatasetInfo:
-        pass
 
-    async def read_data(self, dataset: DatasetInfo, layer_name: str, zoom_step: int, wk_offset: Vec3D, shape: Vec3D) -> \
-            Optional[np.ndarray]:
-        pass
+        path = Path("data", "binary", organization_name, dataset_name)
+        return Dataset(organization_name, dataset_name, TiffDataset(str(path)))
+
+    async def read_data(
+        self,
+        abstract_dataset: DatasetInfo,
+        layer_name: str,
+        zoom_step: int,
+        wk_offset: Vec3D,
+        shape: Vec3D,
+    ) -> Optional[np.ndarray]:
+        dataset = cast(Dataset, abstract_dataset)
+        return dataset.read_data(layer_name, zoom_step, wk_offset, shape)
 
     def clear_dataset_cache(self, dataset: DatasetInfo) -> None:
         pass
--- wkconnect/backends/tiff/models.py
+++ wkconnect/backends/tiff/models.py
@@ -1,17 +1,67 @@
-from dataclasses import InitVar, dataclass, field
+from dataclasses import dataclass
+from typing import Optional
 
-from ..backend import DatasetInfo
+import numpy as np
+from wkcuber.api.Dataset import TiffDataset
+from wkcuber.api.Properties import LayerProperties
+from wkcuber.mag import Mag
+
+from wkconnect.utils.types import Vec3D, Vec3Df
+
+from ...webknossos.models import BoundingBox as WkBoundingBox
 from ...webknossos.models import DataLayer as WkDataLayer
+from ...webknossos.models import DataSource as WkDataSource
+from ...webknossos.models import DataSourceId as WkDataSourceId
+from ..backend import DatasetInfo
+
 
 @dataclass
 class Dataset(DatasetInfo):
+    organization_name: str
+    dataset_name: str
+    dataset_handle: TiffDataset = None
 
+    def __post_init__(self) -> None:
+        print("tiff dataset post init")
 
-    def to_webknossos(self, layer_name: str) -> WkDataLayer:
+    def to_webknossos(self) -> WkDataSource:
+        return WkDataSource(
+            WkDataSourceId(self.organization_name, self.dataset_name),
+            [
+                self.layer_to_webknossos(layer_name, layer)
+                for layer_name, layer in self.dataset_handle.properties.data_layers.items()
+            ],
+            Vec3Df(11.0, 11.0, 24.0),
+        )
+
+    def layer_to_webknossos(
+        self, layer_name: str, layer_properties: LayerProperties
+    ) -> WkDataLayer:
         return WkDataLayer(
             layer_name,
-            "color",
-            bounding_box(),
-            [scale.resolution for scale in self.scales],
-            self.wk_data_type(),
+            layer_properties._category,
+            WkBoundingBox(
+                topLeft=layer_properties._bounding_box["topLeft"],
+                width=layer_properties._bounding_box["width"],
+                height=layer_properties._bounding_box["height"],
+                depth=layer_properties._bounding_box["depth"],
+            ),
+            [
+                Vec3D(
+                    mag.mag.to_array()[0], mag.mag.to_array()[1], mag.mag.to_array()[2]
+                )
+                for mag in layer_properties._wkw_magnifications
+            ],
+            layer_properties._element_class,
         )
+
+    def read_data(
+        self, layer_name: str, zoom_step: int, wk_offset: Vec3D, shape: Vec3D
+    ) -> Optional[np.ndarray]:
+        layer = self.dataset_handle.get_layer(layer_name)
+        mag = list(layer.mags.keys())[zoom_step]
+        mag_dataset = layer.get_mag(mag)
+        offset = (
+            np.array([wk_offset.x, wk_offset.y, wk_offset.z]) / np.array(Mag(mag).mag)
+        ).astype(np.uint32)
+        return mag_dataset.read(shape, tuple(offset))
