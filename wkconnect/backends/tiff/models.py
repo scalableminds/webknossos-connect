dcccfd519d2082e822c80c4d4c118f97ada99348
Florian M <florian@scm.io>
2020-09-22 13:13:02 +0200

    pretty, add assertions

--- wkconnect/backends/tiff/models.py
+++ wkconnect/backends/tiff/models.py
@@ -1,8 +1,9 @@
+import logging
 import math
 from dataclasses import dataclass
 from functools import lru_cache
-from pathlib import Path
 from os import listdir
+from pathlib import Path
 from typing import List, Optional, Tuple
 
 import numpy as np
@@ -16,6 +17,8 @@ from ...webknossos.models import DataSource as WkDataSource
 from ...webknossos.models import DataSourceId as WkDataSourceId
 from ..backend import DatasetInfo
 
+logger = logging.getLogger()
+
 
 @dataclass(frozen=True)
 class Dataset(DatasetInfo):
@@ -26,17 +29,22 @@ class Dataset(DatasetInfo):
     def to_webknossos(self) -> WkDataSource:
         return WkDataSource(
             WkDataSourceId(self.organization_name, self.dataset_name),
-            [self.layer_to_webknossos(layer_name) for layer_name in self.list_layers()],
+            [self.layer_to_webknossos(layer_name) for layer_name in self.layers()],
             self.scale,
         )
 
-    def list_layers(self) -> List[str]:
-        for filename in listdir(str(self.path())):
+    def layers(self) -> List[str]:
+        layers = []
+        path = str(self.path())
+        for filename in listdir(path):
             filepath = Path(filename)
-            if filepath.suffix == '.tif':
-                yield filepath.stem
+            if filepath.suffix == ".tif":
+                layers.append(filepath.stem)
+        if len(layers) == 0:
+            logger.info(f"No valid layers found for tif dataset at {path}")
+        return layers
 
-    def layer_to_webknossos(self, layer_name) -> WkDataLayer:
+    def layer_to_webknossos(self, layer_name: str) -> WkDataLayer:
         mags, tile_shape, page_shapes, dtype, _ = self.read_properties(layer_name)
 
         return WkDataLayer(
@@ -96,8 +104,18 @@ class Dataset(DatasetInfo):
             mags = [Vec3D(2 ** mag, 2 ** mag, 1) for mag in range(len(tif.pages))]
             assert (
                 len(mags) > 0
-            ), f"no magnifications found. empty tif file at {str(filepath)}?"
+            ), f"No magnifications found. Empty tiff at {str(filepath)}?"
             tags = tif.pages[0].tags
+            assert (
+                322 in tags
+                and 323 in tags
+                and 324 in tags
+                and 256 in tags
+                and 257 in tags
+            ), (
+                f"Incomplete tags in tif file at {str(filepath)}."
+                + "Only tiled tifs with resolution pyramid are supported."
+            )
             tile_shape = (tags[322].value, tags[323].value)
             page_shapes = [
                 (page.tags[256].value, page.tags[257].value) for page in tif.pages
@@ -134,7 +152,9 @@ class Dataset(DatasetInfo):
         self, layer_name: str, dtype: np.dtype, byte_offset: int, shape: Tuple[int, int]
     ) -> np.ndarray:
         image = np.zeros(shape, dtype=dtype)
-        mapped = np.memmap(str(self.layer_filepath(layer_name)), dtype, "r", byte_offset, shape, "C")
+        mapped = np.memmap(
+            str(self.layer_filepath(layer_name)), dtype, "r", byte_offset, shape, "C"
+        )
         image[:] = mapped
         del mapped
         return image
