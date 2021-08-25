b604f3456f3246f567cebf2f605f1271db644f5f
Norman Rzepka <code@normanrz.com>
2021-08-25 11:39:33 +0200

    drop murmurhash, because it is not used for volume data

--- wkconnect/backends/neuroglancer/sharding.py
+++ wkconnect/backends/neuroglancer/sharding.py
@@ -1,7 +1,7 @@
 import gzip
 import math
 from dataclasses import dataclass, field
-from typing import Callable, Optional, Tuple, cast
+from typing import Optional, Tuple
 
 import numpy as np
 
@@ -37,215 +37,6 @@ def compressed_morton_code(pos: Vec3D, grid_size: Vec3D) -> np.uint64:
             output_bit += one
 
     return output
-
-
-# flake8: noqa: C901
-def mmh3_128(key: bytes, seed: int = 0x0) -> int:
-    """
-    Implements 128bit murmur3 hash for x86.
-    pymmh3 was written by Fredrik Kihlander and enhanced by Swapnil Gusani.
-    Public domain code. https://code.google.com/p/smhasher/wiki/MurmurHash3
-    """
-
-    def fmix(h: int) -> int:
-        h ^= h >> 16
-        h = (h * 0x85EBCA6B) & 0xFFFFFFFF
-        h ^= h >> 13
-        h = (h * 0xC2B2AE35) & 0xFFFFFFFF
-        h ^= h >> 16
-        return h
-
-    length = len(key)
-    nblocks = int(length / 16)
-
-    h1 = seed
-    h2 = seed
-    h3 = seed
-    h4 = seed
-
-    c1 = 0x239B961B
-    c2 = 0xAB0E9789
-    c3 = 0x38B34AE5
-    c4 = 0xA1E38B93
-
-    # body
-    for block_start in range(0, nblocks * 16, 16):
-        k1 = (
-            key[block_start + 3] << 24
-            | key[block_start + 2] << 16
-            | key[block_start + 1] << 8
-            | key[block_start + 0]
-        )
-
-        k2 = (
-            key[block_start + 7] << 24
-            | key[block_start + 6] << 16
-            | key[block_start + 5] << 8
-            | key[block_start + 4]
-        )
-
-        k3 = (
-            key[block_start + 11] << 24
-            | key[block_start + 10] << 16
-            | key[block_start + 9] << 8
-            | key[block_start + 8]
-        )
-
-        k4 = (
-            key[block_start + 15] << 24
-            | key[block_start + 14] << 16
-            | key[block_start + 13] << 8
-            | key[block_start + 12]
-        )
-
-        k1 = (c1 * k1) & 0xFFFFFFFF
-        k1 = (k1 << 15 | k1 >> 17) & 0xFFFFFFFF  # inlined ROTL32
-        k1 = (c2 * k1) & 0xFFFFFFFF
-        h1 ^= k1
-
-        h1 = (h1 << 19 | h1 >> 13) & 0xFFFFFFFF  # inlined ROTL32
-        h1 = (h1 + h2) & 0xFFFFFFFF
-        h1 = (h1 * 5 + 0x561CCD1B) & 0xFFFFFFFF
-
-        k2 = (c2 * k2) & 0xFFFFFFFF
-        k2 = (k2 << 16 | k2 >> 16) & 0xFFFFFFFF  # inlined ROTL32
-        k2 = (c3 * k2) & 0xFFFFFFFF
-        h2 ^= k2
-
-        h2 = (h2 << 17 | h2 >> 15) & 0xFFFFFFFF  # inlined ROTL32
-        h2 = (h2 + h3) & 0xFFFFFFFF
-        h2 = (h2 * 5 + 0x0BCAA747) & 0xFFFFFFFF
-
-        k3 = (c3 * k3) & 0xFFFFFFFF
-        k3 = (k3 << 17 | k3 >> 15) & 0xFFFFFFFF  # inlined ROTL32
-        k3 = (c4 * k3) & 0xFFFFFFFF
-        h3 ^= k3
-
-        h3 = (h3 << 15 | h3 >> 17) & 0xFFFFFFFF  # inlined ROTL32
-        h3 = (h3 + h4) & 0xFFFFFFFF
-        h3 = (h3 * 5 + 0x96CD1C35) & 0xFFFFFFFF
-
-        k4 = (c4 * k4) & 0xFFFFFFFF
-        k4 = (k4 << 18 | k4 >> 14) & 0xFFFFFFFF  # inlined ROTL32
-        k4 = (c1 * k4) & 0xFFFFFFFF
-        h4 ^= k4
-
-        h4 = (h4 << 13 | h4 >> 19) & 0xFFFFFFFF  # inlined ROTL32
-        h4 = (h1 + h4) & 0xFFFFFFFF
-        h4 = (h4 * 5 + 0x32AC3B17) & 0xFFFFFFFF
-
-    # tail
-    tail_index = nblocks * 16
-    k1 = 0
-    k2 = 0
-    k3 = 0
-    k4 = 0
-    tail_size = length & 15
-
-    if tail_size >= 15:
-        k4 ^= key[tail_index + 14] << 16
-    if tail_size >= 14:
-        k4 ^= key[tail_index + 13] << 8
-    if tail_size >= 13:
-        k4 ^= key[tail_index + 12]
-
-    if tail_size > 12:
-        k4 = (k4 * c4) & 0xFFFFFFFF
-        k4 = (k4 << 18 | k4 >> 14) & 0xFFFFFFFF  # inlined ROTL32
-        k4 = (k4 * c1) & 0xFFFFFFFF
-        h4 ^= k4
-
-    if tail_size >= 12:
-        k3 ^= key[tail_index + 11] << 24
-    if tail_size >= 11:
-        k3 ^= key[tail_index + 10] << 16
-    if tail_size >= 10:
-        k3 ^= key[tail_index + 9] << 8
-    if tail_size >= 9:
-        k3 ^= key[tail_index + 8]
-
-    if tail_size > 8:
-        k3 = (k3 * c3) & 0xFFFFFFFF
-        k3 = (k3 << 17 | k3 >> 15) & 0xFFFFFFFF  # inlined ROTL32
-        k3 = (k3 * c4) & 0xFFFFFFFF
-        h3 ^= k3
-
-    if tail_size >= 8:
-        k2 ^= key[tail_index + 7] << 24
-    if tail_size >= 7:
-        k2 ^= key[tail_index + 6] << 16
-    if tail_size >= 6:
-        k2 ^= key[tail_index + 5] << 8
-    if tail_size >= 5:
-        k2 ^= key[tail_index + 4]
-
-    if tail_size > 4:
-        k2 = (k2 * c2) & 0xFFFFFFFF
-        k2 = (k2 << 16 | k2 >> 16) & 0xFFFFFFFF  # inlined ROTL32
-        k2 = (k2 * c3) & 0xFFFFFFFF
-        h2 ^= k2
-
-    if tail_size >= 4:
-        k1 ^= key[tail_index + 3] << 24
-    if tail_size >= 3:
-        k1 ^= key[tail_index + 2] << 16
-    if tail_size >= 2:
-        k1 ^= key[tail_index + 1] << 8
-    if tail_size >= 1:
-        k1 ^= key[tail_index + 0]
-
-    if tail_size > 0:
-        k1 = (k1 * c1) & 0xFFFFFFFF
-        k1 = (k1 << 15 | k1 >> 17) & 0xFFFFFFFF  # inlined ROTL32
-        k1 = (k1 * c2) & 0xFFFFFFFF
-        h1 ^= k1
-
-    # finalization
-    h1 ^= length
-    h2 ^= length
-    h3 ^= length
-    h4 ^= length
-
-    h1 = (h1 + h2) & 0xFFFFFFFF
-    h1 = (h1 + h3) & 0xFFFFFFFF
-    h1 = (h1 + h4) & 0xFFFFFFFF
-    h2 = (h1 + h2) & 0xFFFFFFFF
-    h3 = (h1 + h3) & 0xFFFFFFFF
-    h4 = (h1 + h4) & 0xFFFFFFFF
-
-    h1 = fmix(h1)
-    h2 = fmix(h2)
-    h3 = fmix(h3)
-    h4 = fmix(h4)
-
-    h1 = (h1 + h2) & 0xFFFFFFFF
-    h1 = (h1 + h3) & 0xFFFFFFFF
-    h1 = (h1 + h4) & 0xFFFFFFFF
-    h2 = (h1 + h2) & 0xFFFFFFFF
-    h3 = (h1 + h3) & 0xFFFFFFFF
-    h4 = (h1 + h4) & 0xFFFFFFFF
-
-    return h4 << 96 | h3 << 64 | h2 << 32 | h1
-
-
-def mmh3_64(key: np.uint64, seed: int = 0x0) -> np.uint64:
-    """ Implements 64bit murmur3 hash. """
-
-    hash_128 = mmh3_128(key.tobytes(), seed)
-
-    unsigned_val1 = hash_128 & 0xFFFFFFFFFFFFFFFF
-    if unsigned_val1 & 0x8000000000000000 == 0:
-        signed_val1 = unsigned_val1
-    else:
-        signed_val1 = -((unsigned_val1 ^ 0xFFFFFFFFFFFFFFFF) + 1)
-
-    # unsigned_val2 = (hash_128 >> 64) & 0xFFFFFFFFFFFFFFFF
-    # if unsigned_val2 & 0x8000000000000000 == 0:
-    #     signed_val2 = unsigned_val2
-    # else:
-    #     signed_val2 = -((unsigned_val2 ^ 0xFFFFFFFFFFFFFFFF) + 1)
-
-    return np.uint64(signed_val1)
 
 
 def identity(a: np.uint64) -> np.uint64:
@@ -299,6 +90,9 @@ class ShardingInfo:
     _minishard_mask: np.uint64 = field(init=False)
 
     def __post_init__(self) -> None:
+        assert (
+            self.hashfn == "identity"
+        ), "Only `identity` is currently supported and not `murmurhash3_x86_128`"
         object.__setattr__(
             self,
             "_shard_mask",
@@ -321,11 +115,8 @@ class ShardingInfo:
 
     def get_minishard_info(self, key: np.uint64) -> MinishardInfo:
         chunk_id = np.uint64(key) >> np.uint64(self.preshift_bits)
-        hashfn = cast(
-            Callable[[np.uint64], np.uint64],
-            mmh3_64 if self.hashfn == "murmurhash3_x86_128" else identity,
-        )
-        chunk_id = hashfn(chunk_id)
+        # Only `identity` hashfn is currently supported, so this is a no-op:
+        # chunk_id = hashfn(chunk_id)
         minishard_number = np.uint64(chunk_id & self._minishard_mask)
         shard_number = np.uint64(
             (chunk_id & self._shard_mask) >> np.uint64(self.minishard_bits)
